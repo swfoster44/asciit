1cbe007989f7fdc4051f8d9bacd0ce26626d23b5
sfoster <stephen.foster.abq@gmail.com>
2024-07-06 19:38:11 -0600

    refactored

--- asciit.go
+++ asciit.go
@@ -2,6 +2,7 @@ package asciit
 
 import (
 	"embed"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -9,26 +10,99 @@ import (
 //go:embed ascii.txt
 var f embed.FS
 
-func Table() map[int]string {
+const (
+	NUMERIC = iota + 100
+	ALPHAL
+	ALPHAU
+	SPACE
+	SYMBOL
+)
+
+type Code = uint64
+
+type Entry struct {
+	Code Code
+	Hex  string
+	Html string
+	Char string
+	Desc string
+	Tag  int
+}
+
+func tagger(c Code) int {
+	switch {
+	case c >= 48 && c <= 57:
+		return NUMERIC
+	case c >= 97 && c <= 122:
+		return ALPHAL
+	case c >= 65 && c <= 90:
+		return ALPHAU
+	case (c >= 8 && c <= 13) || c == 32:
+		return SPACE
+	case c >= 33 && c <= 47:
+		return SYMBOL
+	case c >= 58 && c <= 64:
+		return SYMBOL
+	case c >= 91 && c <= 96:
+		return SYMBOL
+	case c >= 123 && c <= 126:
+		return SYMBOL
+	default:
+		return 0
+	}
+}
+
+type Table struct {
+	codeMap map[Code]*Entry
+	strMap  map[string]*Entry
+}
+
+func (T *Table) ByCode(c Code) *Entry {
+	return T.codeMap[c]
+}
+func (T *Table) ByStr(s string) *Entry {
+	return T.strMap[s]
+}
+
+func newTable() *Table {
+	cm := map[Code]*Entry{}
+	sm := map[string]*Entry{}
+	return &Table{codeMap: cm, strMap: sm}
+}
+
+func New() *Table {
 	data, _ := f.ReadFile("ascii.txt")
 	rows := strings.Split(string(data), "\n")
 
-	ranges := NewRanges()
-	ranges.load(rows)
+	table := newTable()
 
-	table := map[int]string{}
-
-	for i := range ranges.data {
-		row := ranges.data[i]
+	// - 1 because spliting on new line
+	for i := 0; i < len(rows)-1; i++ {
+		row := rows[i]
 		cols := strings.Split(row, " ")
-		codeTrim := strings.TrimSpace(cols[0])
-		code, err := strconv.Atoi(codeTrim)
+
+		code, err := strconv.ParseUint(strings.TrimSpace(cols[0]), 10, 64)
 		if err != nil {
 			panic(err)
 		}
 
-		table[code] = cols[4]
-	}
+		tag := tagger(code)
 
+		if tag != 0 {
+			hex := cols[1]
+			html := cols[3]
+			char := cols[4]
+			var desc string = ""
+
+			if len(cols) > 5 {
+				desc = strings.Join(cols[5:], " ")
+			}
+
+			e := &Entry{code, hex, html, char, desc, tag}
+			table.codeMap[code] = e
+			table.strMap[char] = e
+			// fmt.Printf("%v, %v, %v, %v, %v, %v\n", code, hex, html, char, desc, tag)
+		}
+	}
 	return table
 }
--- asciit_test.go
+++ asciit_test.go
@@ -2,26 +2,8 @@ package asciit
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
-func TestRanges(t *testing.T) {
-	assert := assert.New(t)
-
-	cr := codeRange{48, 57}
-	assert.True(cr.InRange(57))
-	assert.True(cr.InRange(48))
-
-	symbols := symbols_ranges()
-	assert.Len(symbols.crange, 4)
-
-	ranges := NewRanges()
-	assert.Len(ranges.allRanges, 5)
-
-	table := Table()
-	assert.Len(table, 101)
-
-	char := table[97]
-	assert.Equal("a", char)
+func TestTable(t *testing.T) {
+	New()
 }
